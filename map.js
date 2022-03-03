19ad12c5810aea261baa3b1f9f3d5bee51f7fede
stefan-norgaard <spn2121@columbia.edu>
2022-03-03 09:27:53 -0500

    final changes March 3

--- map.js
+++ map.js
@@ -16,6 +16,33 @@ var map = new mapboxgl.Map({
 var blocks_url = "./data/blocks_joined_trees_um.geojson"
 var trees_url = "./data/2015_Street_Tree_Census_subset_um.geojson"
 
+
+// when the user does a 'click' on an element in the 'trees' layer...
+map.on('click', 'trees', function(e) {
+    // get the map coordinates of the feature
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    // get its species name from the feature's attributes
+    var species = e.features[0].properties.spc_common;
+  
+    // and create a popup on the map
+    new mapboxgl.Popup()
+    .setLngLat(coordinates)
+    .setHTML(species)
+    .addTo(map);
+  });
+    
+  // make the cursor a pointer when over the tree
+  map.on('mouseenter', 'trees', function() {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+    
+  // back to normal when it's not
+  map.on('mouseleave', 'trees', function() {
+    map.getCanvas().style.cursor = '';
+  });
+
+  
+
 map.on('load',function(){
     // define a 'source' for your polygons dataset
     map.addSource('blocks_data',{
@@ -80,4 +107,12 @@ map.addSource('trees_data',{
       'circle-opacity':0.7,
       'circle-radius':4
     },
-  })
+})
+
+// listen for 'onclick' events from 'handler-element'
+document.getElementById('handler-element').onclick = handleEvent;
+
+// say how to handle the events that are fired by the listener
+function handleEvent(e) {
+  console.log("an event happened")
+}
